2e4f6d1274a1ba87b8c3398ff8369a1f0e937813
wabu <waldek.bus@gmail.com>
2022-12-26 14:35:55 +0100

    Currency Conversion Client Side Load Balancing

--- currency-conversion-service/src/main/java/pl/wabu/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/pl/wabu/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.wabu.currencyconversionservice.model.CurrencyConversion;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+// remove the URL to let Feign get available instances of the Currency-Exchange from Eureka
+@FeignClient(name = "currency-exchange")
+
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
